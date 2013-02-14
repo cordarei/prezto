898fab620c5ec91a4f31a5a9faed41945221061b
Joseph Irwin <joseph.irwin.gt@gmail.com>
2013-02-14 14:20:05 +0900

    Don't start new ssh-agent if X session starts one
    Either Ubuntu or Xfce starts an instance of ssh-agent on login; this
    commit alters the ssh-agent module to not start a new process if one
    already exists. If an ssh-agent process exists and the environment
    variables are available then we create the prezto/ssh-agent session file
    by hand and load identities normally.

--- modules/ssh-agent/init.zsh
+++ modules/ssh-agent/init.zsh
@@ -22,14 +22,8 @@ pmodload 'helper'
 _ssh_agent_env="${HOME}/.ssh/environment-${HOST}"
 _ssh_agent_forwarding=
 
-function _ssh-agent-start {
+function _ssh-agent-load-identites {
   local -a identities
-
-  # Start ssh-agent and setup the environment.
-  rm -f "${_ssh_agent_env}"
-  ssh-agent > "${_ssh_agent_env}"
-  chmod 600 "${_ssh_agent_env}"
-  source "${_ssh_agent_env}" > /dev/null
 
   # Load identities.
   zstyle -a ':prezto:module:ssh-agent' identities 'identities'
@@ -41,6 +35,32 @@ function _ssh-agent-start {
   fi
 }
 
+function _ssh-agent-start {
+  # Start ssh-agent and setup the environment.
+  rm -f "${_ssh_agent_env}"
+  ssh-agent > "${_ssh_agent_env}"
+  chmod 600 "${_ssh_agent_env}"
+  source "${_ssh_agent_env}" > /dev/null
+
+  _ssh-agent-load-identites
+}
+
+# Check for an instance of ssh-agent that may have been started by the X
+# session, and pretend that it was started by prezto.
+if [[ ! -s "${_ssh_agent_env}" ]]; then
+  if [[ -n "$SSH_AGENT_PID" ]]; then
+    ps -e | grep "${SSH_AGENT_PID}" | grep -q 'ssh-agent$'
+    if (( $? == 0 )); then
+      cat >"${_ssh_agent_env}" <<EOS
+SSH_AGENT_PID=$SSH_AGENT_PID; export SSH_AGENT_PID
+SSH_AUTH_SOCK=$SSH_AUTH_SOCK; export SSH_AUTH_SOCK
+echo Agent pid $SSH_AGENT_PID
+EOS
+      _ssh-agent-load-identites
+    fi
+  fi
+fi
+
 # Test if agent-forwarding is enabled.
 zstyle -b ':prezto:module:ssh-agent' forwarding '_ssh_agent_forwarding'
 if is-true "${_ssh_agent_forwarding}" && [[ -n "$SSH_AUTH_SOCK" ]]; then
@@ -49,7 +69,7 @@ if is-true "${_ssh_agent_forwarding}" && [[ -n "$SSH_AUTH_SOCK" ]]; then
 elif [[ -s "${_ssh_agent_env}" ]]; then
   # Source SSH settings, if applicable.
   source "${_ssh_agent_env}" > /dev/null
-  ps -ef | grep "${SSH_AGENT_PID}" | grep -q 'ssh-agent$' || {
+  ps -e | grep "${SSH_AGENT_PID}" | grep -q 'ssh-agent$' || {
     _ssh-agent-start;
   }
 else
@@ -58,5 +78,6 @@ fi
 
 # Tidy up after ourselves.
 unfunction _ssh-agent-start
+unfunction _ssh-agent-load-identites
 unset _ssh_agent_{env,forwarding}
 
